1eee595b894bd76eb07e2f3ccbd136d362dcf4dc
Shawn Wilsher <656602+sdwilsh@users.noreply.github.com>
2025-05-11 09:35:54 -0700

    feat: install shell dependencies

--- build_files/build.sh
+++ build_files/build.sh
@@ -5,5 +5,7 @@ set -ouex pipefail
 ### Setup Alloy Container
 rsync -rvK /ctx/alloy/ /
 
+/ctx/packages.sh
+
 /ctx/branding.sh
 /ctx/cleanup.sh
---
+++ build_files/packages.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -ouex pipefail
+
+dnf -y copr enable atim/starship
+dnf -y copr enable sramanujam/atuin
+
+CHEZMOI_VERSION=2.62.3
+
+dnf -y install \
+    atuin \
+    fish \
+    starship \
+    https://github.com/twpayne/chezmoi/releases/download/v${CHEZMOI_VERSION}/chezmoi-${CHEZMOI_VERSION}-$(arch).rpm
