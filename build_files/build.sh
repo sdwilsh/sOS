a05df1616e3660442cf9882f70abf91c0061d721
Shawn Wilsher <656602+sdwilsh@users.noreply.github.com>
2025-05-15 22:57:22 -0700

    feat: add a node-exporter container

--- build_files/build.sh
+++ build_files/build.sh
@@ -8,6 +8,9 @@ set -ouex pipefail
 rsync -rvK /ctx/alloy/ /
 chown 473:473 /etc/alloy/journal.alloy
 
+### Setup Node Exporter Container
+rsync -rvK /ctx/node-exporter/ /
+
 /ctx/packages.sh
 
 /ctx/branding.sh
